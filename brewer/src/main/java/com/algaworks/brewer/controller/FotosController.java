ec8157469d337e41a492fd75500357466e0d91a3
Leandro Neis <lgn200@gmail.com>
2022-06-17 00:37:15 -0300

    14.4. Melhorando a disponibilidade da aplicação - retorno assíncrono

--- brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -1,7 +1,9 @@
 package com.algaworks.brewer.controller;
 
+import com.algaworks.brewer.storage.FotoStorageRunnable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -9,8 +11,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class FotosController {
 
     @PostMapping
-    public String upload(@RequestParam("files[]")  MultipartFile[] files){
-        return "Ok!";
+    public DeferredResult<String> upload(@RequestParam("files[]")  MultipartFile[] files){
+        DeferredResult<String> resultado = new DeferredResult<>();
+
+        Thread thread = new Thread(new FotoStorageRunnable(files,resultado));
+        thread.start();
+
+        return resultado;
     }
 
 }
