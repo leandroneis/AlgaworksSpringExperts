f5b1193914a075efd15650d20306c26ca11f4474
leandro <lgn200@gmail.com>
2022-06-19 23:36:40 -0300

    14.7. Salvando a imagem temporária

--- brewer/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ brewer/src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 public class FotoStorageLocal implements FotoStorage {
     private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
@@ -26,6 +28,20 @@ public class FotoStorageLocal implements FotoStorage {
         criarPastas();
     }
 
+    @Override
+    public String salvarTemporariamente(MultipartFile[] files) {
+        String novoNome = null;
+        if(files != null && files.length > 0) {
+            MultipartFile arquivo = files[0];
+            novoNome = renomearArquivo(arquivo.getOriginalFilename());
+            try {
+                arquivo.transferTo(new File(this.localtemporario.toAbsolutePath().toString()+ getDefault().getSeparator() + novoNome));
+            } catch (IOException e) {
+                throw new RuntimeException("Erro salvando a foto na pasta temporária",e);
+            }
+        }
+        return novoNome;
+    }
     private void criarPastas() {
         try {
             Files.createDirectories(this.local);
@@ -42,8 +58,12 @@ public class FotoStorageLocal implements FotoStorage {
         }
     }
 
-    @Override
-    public void salvarTemporariamente(MultipartFile[] files) {
-        System.out.println(">> Salvando a foto temporariamente....");
+    private String renomearArquivo(String nomeOrinal){
+        String novoNome = UUID.randomUUID().toString() + "_" + nomeOrinal;
+        if(logger.isDebugEnabled()){
+            logger.debug(String.format("Nome original: %s,novo nome do arquivo: %s",nomeOrinal,novoNome));
+        }
+        return novoNome;
     }
+
 }
