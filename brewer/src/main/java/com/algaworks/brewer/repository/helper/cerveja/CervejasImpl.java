d28cfe9f6d1cd99401653d1136b747e86b453ad7
Leandro Gustavo Neis <lgn200@gmail.com>
2022-09-05 15:29:35 -0300

    15.13. Parâmetros da ordenação

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -5,13 +5,11 @@ import javax.persistence.PersistenceContext;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Projection;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -34,6 +32,14 @@ public class CervejasImpl implements CervejasQueries {
 
         criteria.setFirstResult(primeiroRegistro);
         criteria.setMaxResults(totalRegistrosPorPagina);
+
+        Sort sort = pageable.getSort();
+
+        if(sort != null){
+            Sort.Order order = sort.iterator().next();
+            String property = order.getProperty();
+            criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
+        }
 
         adicionarFiltro(filtro, criteria);
 
