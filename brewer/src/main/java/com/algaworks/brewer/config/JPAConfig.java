2ff2eaa9e99c19d49189d10abba5b1776f7ea28b
Leandro Gustavo Neis <lgn200@gmail.com>
2022-10-24 21:53:04 -0300

    15.18. Bean útil para paginação

--- brewer/src/main/java/com/algaworks/brewer/config/JPAConfig.java
+++ brewer/src/main/java/com/algaworks/brewer/config/JPAConfig.java
@@ -3,7 +3,9 @@ package com.algaworks.brewer.config;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import com.algaworks.brewer.controller.CervejasController;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
+@ComponentScan(basePackageClasses = { Cervejas.class })
 @EnableJpaRepositories(basePackageClasses = Cervejas.class,enableDefaultTransactions = false)
 @EnableTransactionManagement
 public class JPAConfig {
--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -3,13 +3,15 @@ package com.algaworks.brewer.repository.helper.cerveja;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -21,25 +23,16 @@ public class CervejasImpl implements CervejasQueries {
     @PersistenceContext
     private EntityManager manager;
 
-    @SuppressWarnings("unchecked")
+    @Autowired
+    private PaginacaoUtil paginacaoUtil;
+
+
     @Override
     @Transactional(readOnly = true)
     public Page<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable) {
         Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
-        int paginaAtual = pageable.getPageNumber();
-        int totalRegistrosPorPagina = pageable.getPageSize();
-        int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
 
-        criteria.setFirstResult(primeiroRegistro);
-        criteria.setMaxResults(totalRegistrosPorPagina);
-
-        Sort sort = pageable.getSort();
-
-        if(sort != null){
-            Sort.Order order = sort.iterator().next();
-            String property = order.getProperty();
-            criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
-        }
+        paginacaoUtil.preparar(criteria,pageable);
 
         adicionarFiltro(filtro, criteria);
 
