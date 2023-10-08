ca37c5b1bde6f5c77ba19deedc991836eeabe8e2
leandro <lgn200@gmail.com>
2023-10-08 17:25:26 -0300

    21.3.Trabalhando no filtro por grupo

--- brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/Usuarios.java
@@ -1,7 +1,9 @@
 package com.algaworks.brewer.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.repository.helper.usuario.UsuariosQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +13,5 @@ public interface Usuarios extends JpaRepository<Usuario, Long>, UsuariosQueries 
 
     public Optional<Usuario> findByEmail(String email);
 
+     public List<Usuario> filtrar(UsuarioFilter filtro);
 }
