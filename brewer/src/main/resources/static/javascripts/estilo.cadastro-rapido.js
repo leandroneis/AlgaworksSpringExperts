8a2c9abeaf8c6faa447127b4b438640c2e2506d2
Leandro Gustavo Neis <lgn200@gmail.com>
2023-10-31 19:47:04 -0300

    22.4. Selecionando o cliente na pesquisa rápida

--- brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
+++ brewer/src/main/resources/static/javascripts/estilo.cadastro-rapido.js
@@ -1,7 +1,7 @@
 var Brewer = Brewer || {};
 
 Brewer.EstiloCadastroRapido = (function() {
-	
+
 	function EstiloCadastroRapido() {
 		this.modal = $('#modalCadastroRapidoEstilo');
 		this.botaoSalvar = this.modal.find('.js-modal-cadastro-estilo-salvar-btn');
@@ -10,24 +10,24 @@ Brewer.EstiloCadastroRapido = (function() {
 		this.inputNomeEstilo = $('#nomeEstilo');
 		this.containerMensagemErro = $('.js-mensagem-cadastro-rapido-estilo');
 	}
-	
+
 	EstiloCadastroRapido.prototype.iniciar = function() {
 		this.form.on('submit', function(event) { event.preventDefault() });
 		this.modal.on('shown.bs.modal', onModalShow.bind(this));
 		this.modal.on('hide.bs.modal', onModalClose.bind(this))
 		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
 	}
-	
+
 	function onModalShow() {
 		this.inputNomeEstilo.focus();
 	}
-	
+
 	function onModalClose() {
 		this.inputNomeEstilo.val('');
 		this.containerMensagemErro.addClass('hidden');
 		this.form.find('.form-group').removeClass('has-error');
 	}
-	
+
 	function onBotaoSalvarClick() {
 		var nomeEstilo = this.inputNomeEstilo.val().trim();
 		$.ajax({
@@ -39,23 +39,23 @@ Brewer.EstiloCadastroRapido = (function() {
 			success: onEstiloSalvo.bind(this)
 		});
 	}
-	
+
 	function onErroSalvandoEstilo(obj) {
 		var mensagemErro = obj.responseText;
 		this.containerMensagemErro.removeClass('hidden');
 		this.containerMensagemErro.html('<span>' + mensagemErro + '</span>');
 		this.form.find('.form-group').addClass('has-error');
 	}
-	
+
 	function onEstiloSalvo(estilo) {
 		var comboEstilo = $('#estilo');
 		comboEstilo.append('<option value=' + estilo.codigo + '>' + estilo.nome + '</option>');
 		comboEstilo.val(estilo.codigo);
 		this.modal.modal('hide');
 	}
-	
+
 	return EstiloCadastroRapido;
-	
+
 }());
 
 $(function() {
