b766af64b89db18d9378b61b5a83e4f072915151
Leandro <lgn200@gmail.com>
2024-01-01 20:06:45 -0300

    23.12. Atualizando valor total^

--- brewer/src/main/resources/static/javascripts/brewer.js
+++ brewer/src/main/resources/static/javascripts/brewer.js
@@ -1,57 +1,57 @@
 var Brewer = Brewer || {};
 
 Brewer.MaskMoney = (function() {
-	
+
 	function MaskMoney() {
 		this.decimal = $('.js-decimal');
 		this.plain = $('.js-plain');
 	}
-	
+
 	MaskMoney.prototype.enable = function() {
 		this.decimal.maskMoney({ decimal: ',', thousands: '.' });
 		this.plain.maskMoney({ precision: 0, thousands: '.' });
 	}
-	
+
 	return MaskMoney;
-	
+
 }());
 
 Brewer.MaskPhoneNumber = (function() {
-	
+
 	function MaskPhoneNumber() {
 		this.inputPhoneNumber = $('.js-phone-number');
 	}
-	
+
 	MaskPhoneNumber.prototype.enable = function() {
 		var maskBehavior = function (val) {
-		  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
 		};
-		
+
 		var options = {
-		  onKeyPress: function(val, e, field, options) {
-		      field.mask(maskBehavior.apply({}, arguments), options);
-		    }
+			onKeyPress: function(val, e, field, options) {
+				field.mask(maskBehavior.apply({}, arguments), options);
+			}
 		};
-		
+
 		this.inputPhoneNumber.mask(maskBehavior, options);
 	}
-	
+
 	return MaskPhoneNumber;
-	
+
 }());
 
 Brewer.MaskCep = (function() {
-	
+
 	function MaskCep() {
 		this.inputCep = $('.js-cep');
 	}
-	
+
 	MaskCep.prototype.enable = function() {
 		this.inputCep.mask('00.000-000');
 	}
-	
+
 	return MaskCep;
-	
+
 }());
 
 Brewer.MaskDate = (function() {
@@ -90,18 +90,23 @@ Brewer.Security = (function() {
 
 }());
 
+numeral.language('pt-br');
+
 Brewer.formatarMoeda = function(valor) {
-	numeral.language('pt-br');
 	return numeral(valor).format('0,0.00');
+}
+
+Brewer.recuperarValor = function(valorFormatado) {
+	return numeral().unformat(valorFormatado);
 }
 
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
-	
+
 	var maskPhoneNumber = new Brewer.MaskPhoneNumber();
 	maskPhoneNumber.enable();
-	
+
 	var maskCep = new Brewer.MaskCep();
 	maskCep.enable();
 
@@ -110,5 +115,5 @@ $(function() {
 
 	var security = new Brewer.Security();
 	security.enable();
-	
+
 });
